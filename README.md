b8ebe79da0fbcf3bca67d45ecce6107318b2bb57
Amanda-Cole <col22031@byui.edu>
2023-10-12 19:30:26 -0600

    Finished To-Do List

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# To-DoList
-To Do List in JavaScript
+# Overview
+
+Using JavaScript, HTML, and CSS I have created a basic todo list program that allows you to add, edit, and delete items from your list.
+
+The software that I wrote uses functions and eventListeners to ensure the user has a pleasant experience with the program.  The program begins when the addButton is clicked.  The ability to edit and delete the added entry is then open for the user to select.
+
+This program allowed me to practice writing a functional webbased application that used HTML, CSS, and JavaScript.  I was able to practice design, functionality, and the ability to call HTML Elments in JavaScript. 
+
+{Provide a link to your YouTube demonstration. It should be a 4-5 minute demo of the software running and a walkthrough of the code. Focus should be on sharing what you learned about the language syntax.}
+
+[Software Demo Video](http://youtube.link.goes.here)
+
+# Development Environment
+
+I used VisualStudio Code to write this program.
+
+I used HTML, CSS, and JavaScript to write this program. 
+
+# Useful Websites
+
+
+- [w3 Schools](https://www.w3schools.com/w3css/default.asp)
+- [mdn web docs](https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/JavaScript_basics)
+
+# Future Work
+
+
+- Allow more time for research when working with HTML, CSS, and JavaScript
+- Create working program before focusing on "looks"
+- Create bookmarks for helpful resources
---
+++ ToDoList.js
@@ -0,0 +1,80 @@
+
+// setup textBox that receives input,addbutton that adds to todo List, and container shows previous input
+const textBox = document.querySelector('#text-box');
+const addButton = document.querySelector('#add-button');
+const todosContainer = document.querySelector('.todos-container');
+
+function checkInput(input){
+    if(input.trim() === ""){
+        throw new Error("Input cannot be Empty");
+    }
+}
+
+addButton.addEventListener('click', ()=>{
+    try{
+        if(textBox.value.trim() === '')
+        {
+            throw new Error("Input dont right");
+        }
+
+    // if(textBox.value.trim().length == ' ')
+    // return;
+
+        //create div add class todo-item-container
+        const todoItemContainer = document.createElement('div');
+        todoItemContainer.classList.add('todo-item-container');
+
+        //add the todo-item-container to the todos container
+        todosContainer.appendChild(todoItemContainer);
+
+        //create p element add id = todo-text
+        const todoText = document.createElement('p');
+        todoText.id = 'todo-text';
+        todoText.innerText = textBox.value;
+        todoItemContainer.appendChild(todoText);
+        //add double click to todoText
+        todoText.addEventListener('dblclick', ()=>{
+            todoText.classList.add('line-through');
+            editButton.setAttribute("disabled","disabled")
+        });
+
+        //create edit button add i = edit-button, add to todoText in todoItemContainer
+        const editButton = document.createElement('button');
+        editButton.id = 'edit-button';
+        //create img add to button
+        const editImage = document.createElement('img');
+        editImage.src = 'edit-colored.png';
+        editButton.appendChild(editImage);
+        todoItemContainer.appendChild(editButton);
+        //add click event to edit button
+        editButton.addEventListener('click', ()=>{
+            textBox.value = todoText.innerText;
+            const parent = editButton.parentElement;
+            parent.parentElement.removeChild(parent);
+        });
+
+        //create delete button
+        const deleteButton = document.createElement('button');
+        deleteButton.id = 'delete-button';
+        //create img add to button
+        const deleteImage = document.createElement('img');
+        deleteImage.src = 'delete-colored.png';
+        deleteButton.appendChild(deleteImage);
+        todoItemContainer.appendChild(deleteButton);
+
+        deleteButton.addEventListener('click', ()=>{
+            const parent = deleteButton.parentElement;
+            parent.parentElement.removeChild(parent);
+        });
+        //clear textBox after Button Click
+        textBox.value = "";
+        //cursor active in text box
+        textBox.focus();
+    }
+    catch (error) {
+        alert("Must input a valid item")
+        console.error('An error occurred:', error.message);
+    
+
+    }
+});
